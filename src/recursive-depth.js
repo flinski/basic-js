a39ad52811f223028cf1f0651ac0293cbe47bb17
Vladislav Flinski <flinssski@gmail.com>
2025-01-02 18:33:45 +0300

    feat: add solutions

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,11 +1,11 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require('../extensions/index.js')
 
 /**
  * Implement class DepthCalculator with method calculateDepth
  * that calculates deoth of nested array
- * 
+ *
  * @example
- * 
+ *
  * const depthCalc = new DepthCalculator();
  * depthCalc.calculateDepth([1, 2, 3, 4, 5]) => 1
  * depthCalc.calculateDepth([1, 2, 3, [4, 5]]) => 2
@@ -13,12 +13,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	calculateDepth(arr) {
+		let depth = 1
+
+		for (const item of arr) {
+			if (Array.isArray(item)) {
+				const deeperDepth = this.calculateDepth(item)
+				depth = Math.max(deeperDepth + 1, depth)
+			}
+		}
+
+		return depth
+	}
 }
 
 module.exports = {
-  DepthCalculator
-};
+	DepthCalculator,
+}
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,23 +1,66 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require('../extensions/index.js')
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+	if (!Array.isArray(arr)) {
+		throw new Error("'arr' parameter must be an instance of the Array!")
+	}
+
+	const transformedArr = []
+
+	for (let i = 0; i < arr.length; i += 1) {
+		const item = arr[i]
+
+		switch (item) {
+			case '--discard-next': {
+				if (i + 1 === arr.length) {
+					continue
+				}
+				i += 2
+				break
+			}
+			case '--discard-prev': {
+				if (i === 0) {
+					continue
+				}
+				transformedArr.pop()
+				break
+			}
+			case '--double-next': {
+				if (i + 1 === arr.length) {
+					continue
+				}
+				transformedArr.push(arr[i + 1])
+				break
+			}
+			case '--double-prev': {
+				if (i === 0) {
+					continue
+				}
+				transformedArr.push(arr[i - 1])
+				break
+			}
+			default: {
+				transformedArr.push(item)
+			}
+		}
+	}
+
+	return transformedArr
 }
 
 module.exports = {
-  transform
-};
+	transform,
+}
